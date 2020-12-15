46b1ae30bcad99188a3bff8647aec8e5ca756de1
Giomar Osorio <giosorio30@gmail.com>
2020-12-15 09:36:04 -0400

    more fixes2

--- install_arch.sh
+++ install_arch.sh
@@ -42,19 +42,12 @@ selectdisk(){
 # --------------------------------------------------------
 selectswapsize(){
     echo "################################################################"
-    echo "SELECTING HARD DISK"
+    echo "SELECTING SWAP SIZE"
     echo "################################################################"
-	items=$(lsblk -d -p -n -l -o NAME,SIZE -e 7,11)
-	options=()
-	IFS_ORIG=$IFS
-	IFS=$'\n'
-	for item in ${items}
-	do  
-			options+=("${item}" "")
-	done
-	IFS=$IFS_ORIG
-	hd=$(whiptail --backtitle "Arch Install Script" --title "${title_hd}" --menu "{$menu_hd}" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
-	echo ${hd%%\ *}   
+    swap_size=$(whiptail --backtitle "Arch Install Script" --inputbox "${menu_swap}" 8 39 ${default_swap_size} --title "${title_swap}" 3>&1 1>&2 2>&3)
+    [[ $swap_size =~ ^[0-9]+$ ]] || swap_size=$default_size
+	#hd=$(whiptail --backtitle "Arch Install Script" --title "${title_hd}" --menu "{$menu_hd}" 0 0 0 "${options[@]}" 3>&1 1>&2 2>&3)
+	echo ${swap_size%%\ *}   
 }
 # --------------------------------------------------------
 pressanykey(){
@@ -68,14 +61,21 @@ loadconfigs(){
     # editor ---------------------------------------------
     editor = "vim"
 
-    # hard disk ---------------------------------------------
+    # hard disk ------------------------------------------
+    hd = ""
     title_hd = "Choose your hard drive"
     menu_hd = "Where do you want to install your new system?\n\nSelect with SPACE, valid with ENTER.\n\nWARNING: Everything will be DESTROYED on the hard disk!"
-    hd = ""
-    default_swap_size = "6"
     
-    # hard disk ---------------------------------------------
+    
+    #Swap ------------------------------------------------
+    default_swap_size = "6"
+    title_swap = "Choose your swap size"
+    menu_swap = "The boot will be 512M\nThe root will be the rest of the hard disk\nEnter partitionsize in gb for the Swap. \n\nIf you dont enter anything: \nswap -> ${default_size}G \n\n"
+    
+    
+    # hard disk ------------------------------------------
     txtpressanykey="Press any key to continue."
 }
 
+loadconfigs
 selectdisk
