470806f3e623ac9e45aefa58dcc79ff8b5e690c3
Giomar Osorio <giosorio30@gmail.com>
2021-01-22 14:41:15 -0400

    fixes references google chrome

--- .config/qtile/bindings.py
+++ .config/qtile/bindings.py
@@ -1,4 +1,4 @@
-'''
+"""
 My mouse and key bindings.
 
 String names for non-alpha-numeric keys can be found here:
@@ -20,7 +20,7 @@ I'm not being 100% consistent but in general:
 Anything bound to arrow keys is movement based. I'm having problems binding
 `M-C={h,j,k,l}` which is preventing me using that for movement. (Though this
 may be something to do with my own ez_keys function...!)
-'''
+"""
 import os
 import re
 
@@ -45,10 +45,11 @@ from groups import groups
 #    command = "setxkbmap {}".format(K_LAYOUTS[next_layout])
 #    run(command, with_output=False)
 
+
 def toggle_klayout(qtile):
     """Change the keyboard layout taking into account the positions defined in K_LAYOUTS"""
-    query = run('setxkbmap -print', with_output=True)
-    search_layout = re.search('\+(.*)\+', query).group(1)
+    query = run("setxkbmap -print", with_output=True)
+    search_layout = re.search("\+(.*)\+", query).group(1)
     current_layout = re.sub(r"\)", "", re.sub(r"\(", " ", search_layout))
     next_layout = K_LAYOUTS.index(current_layout) + 1
     if next_layout >= len(K_LAYOUTS):
@@ -58,20 +59,22 @@ def toggle_klayout(qtile):
 
 
 def to_scratchpad(window):
-    '''
+    """
     Mark the current window as a scratchpad. This resises it, sets it to
     floating and moves it to the hidden `scratchpad` group.
-    '''
+    """
     try:
-        window.togroup('scratchpad')
+        window.togroup("scratchpad")
         window.on_scratchpad = True
     except Exception as e:
         # No `scratchpad` group
-        notify((
-            'You are attempting to use scratchpads without a `scratchpad`'
-            ' group being defined! Define one in your config and restart'
-            ' qtile to enable scratchpads.'
-        ))
+        notify(
+            (
+                "You are attempting to use scratchpads without a `scratchpad`"
+                " group being defined! Define one in your config and restart"
+                " qtile to enable scratchpads."
+            )
+        )
 
     window.floating = True
     screen = window.group.screen
@@ -85,24 +88,26 @@ def to_scratchpad(window):
 
 
 def show_scratchpad(qtile):
-    '''
+    """
     Cycle through any current scratchpad windows on the current screen.
-    '''
-    scratchpad = qtile.groupMap.get('scratchpad')
+    """
+    scratchpad = qtile.groupMap.get("scratchpad")
     if scratchpad is None:
-        notify((
-            'You are attempting to use scratchpads without a `scratchpad`'
-            ' group being defined! Define one in your config and restart'
-            ' qtile to enable scratchpads.'
-        ))
+        notify(
+            (
+                "You are attempting to use scratchpads without a `scratchpad`"
+                " group being defined! Define one in your config and restart"
+                " qtile to enable scratchpads."
+            )
+        )
 
     for w in list(qtile.currentGroup.windows):
-        if not hasattr(w, 'on_scratchpad'):
+        if not hasattr(w, "on_scratchpad"):
             # Ensure that we don't get an attribute error
             w.on_scratchpad = False
 
         if w.on_scratchpad:
-            w.togroup('scratchpad')
+            w.togroup("scratchpad")
 
     if scratchpad.focusHistory:
         # We have at least one scratchpad window to display so show that last
@@ -114,92 +119,106 @@ def show_scratchpad(qtile):
 
 # qtile actually has an emacs style `EzKey` helper that makes specifying
 # key bindings a lot nicer than the default.
-keys = [Key(k[0], k[1], k[2]) for k in [
-    # .: Movement :.
-    # Swtich focus between panes
-    ([MOD], "Up", lazy.layout.up()),
-    ([MOD], "Down", lazy.layout.down()),
-    ([MOD], "Left", lazy.layout.left()),
-    ([MOD], "Right", lazy.layout.right()),
-
-    ([MOD], "h", lazy.layout.left()),
-    ([MOD], "j", lazy.layout.down()),
-    ([MOD], "k", lazy.layout.up()),
-    ([MOD], "l", lazy.layout.right()),
-
-    # Swap panes: target relative to active.
-    # NOTE :: The `swap` commands are for XMonad
-    ([MOD, "shift"], "Up", lazy.layout.shuffle_up()),
-    ([MOD, "shift"], "Down", lazy.layout.shuffle_down()),
-    ([MOD, "shift"], "Left", lazy.layout.shuffle_left(), lazy.layout.swap_left()),
-    ([MOD, "shift"], "Right", lazy.layout.shuffle_right(), lazy.layout.swap_right()),
-
-    ([MOD, "shift"], "h", lazy.layout.shuffle_left(), lazy.layout.swap_left()),
-    ([MOD, "shift"], "j", lazy.layout.shuffle_down()),
-    ([MOD, "shift"], "k", lazy.layout.shuffle_up()),
-    ([MOD, "shift"], "l", lazy.layout.shuffle_right(), lazy.layout.swap_right()),
-
-    ## .: Program Launchers :. #
-    ([MOD], "Return", lazy.spawn(TERMINAL)),
-    ([MOD, "shift"], "Return", lazy.spawn(
-        "dmenu_run -b -p 'λ' -sb '{}' -sf '{}' -nb '{}' -nf '{}'".format(
-            COLOR_SCHEME["selected"],
-            COLOR_SCHEME["active_group"],
-            COLOR_SCHEME["background"],
-            COLOR_SCHEME["foreground"],)
-    )
-    ),
-    ([MOD], "b", lazy.spawn("google-chrome-stable")),
-    ([MOD], "r", lazy.spawn(TERMINAL + ' -e "ranger"')),
-    ([MOD], "d", lazy.spawn("thunar")),
-    ([MOD], "m", lazy.spawn(TERMINAL + ' -e "ncmpcpp"')),
-    ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
-    ([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
-    ([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
-    (["shift"], "F12", lazy.spawn("pamixer --decrease 5")),
-    (["shift"], "F11", lazy.spawn("pamixer --increase 5")),
-    ([], "Print", lazy.spawn("flameshot gui")),
-
-    # Scratchpad toggles
-    #("M-<slash>", lazy.group['scratchpad'].dropdown_toggle('term')),
-    #("M-S-<slash>", lazy.group['scratchpad'].dropdown_toggle('ipython')),
-    ## ("M-<slash>", lazy.window.function(to_scratchpad)),
-    ## ("M-S-<slash>", lazy.function(show_scratchpad)),
-
-    ## .: Layout / Focus Manipulation :. #
-    ([MOD], "f", lazy.window.toggle_fullscreen()),
-    # Toggle between the available layouts.
-    ([MOD], "Tab", lazy.next_layout()),
-    ([MOD, "shift"], "Tab", lazy.prev_layout()),
-    # Close the current window: NO WARNING!
-    ([MOD], "w", lazy.window.kill()),
-
-    ## .: Sys + Utils :. #
-    # Restart qtile in place and pull in config changes (check config before
-    # doing this with `check-qtile-conf` script to avoid crashes)
-    ([MOD, "control"], "r", lazy.restart()),
-    # Shut down qtile.
-    ([MOD, "control"], "Escape", lazy.shutdown("")),
-    ([MOD, "control"], "l", lazy.spawn("betterlockscreen --off 15 -t 'LOCKED' -l")),
-    ([MOD], "space", lazy.function(toggle_klayout))
-    #([MOD,"shift"],"Delete", lazy.spawn(script("power-menu.sh"))),
-
-]]
+keys = [
+    Key(k[0], k[1], k[2])
+    for k in [
+        # .: Movement :.
+        # Swtich focus between panes
+        ([MOD], "Up", lazy.layout.up()),
+        ([MOD], "Down", lazy.layout.down()),
+        ([MOD], "Left", lazy.layout.left()),
+        ([MOD], "Right", lazy.layout.right()),
+        ([MOD], "h", lazy.layout.left()),
+        ([MOD], "j", lazy.layout.down()),
+        ([MOD], "k", lazy.layout.up()),
+        ([MOD], "l", lazy.layout.right()),
+        # Swap panes: target relative to active.
+        # NOTE :: The `swap` commands are for XMonad
+        ([MOD, "shift"], "Up", lazy.layout.shuffle_up()),
+        ([MOD, "shift"], "Down", lazy.layout.shuffle_down()),
+        ([MOD, "shift"], "Left", lazy.layout.shuffle_left(), lazy.layout.swap_left()),
+        (
+            [MOD, "shift"],
+            "Right",
+            lazy.layout.shuffle_right(),
+            lazy.layout.swap_right(),
+        ),
+        ([MOD, "shift"], "h", lazy.layout.shuffle_left(), lazy.layout.swap_left()),
+        ([MOD, "shift"], "j", lazy.layout.shuffle_down()),
+        ([MOD, "shift"], "k", lazy.layout.shuffle_up()),
+        ([MOD, "shift"], "l", lazy.layout.shuffle_right(), lazy.layout.swap_right()),
+        ## .: Program Launchers :. #
+        ([MOD], "Return", lazy.spawn(TERMINAL)),
+        (
+            [MOD, "shift"],
+            "Return",
+            lazy.spawn(
+                "dmenu_run -b -p 'λ' -sb '{}' -sf '{}' -nb '{}' -nf '{}'".format(
+                    COLOR_SCHEME["selected"],
+                    COLOR_SCHEME["active_group"],
+                    COLOR_SCHEME["background"],
+                    COLOR_SCHEME["foreground"],
+                )
+            ),
+        ),
+        ([MOD], "b", lazy.spawn("firefox")),
+        ([MOD], "r", lazy.spawn(TERMINAL + ' -e "ranger"')),
+        ([MOD], "d", lazy.spawn("thunar")),
+        ([MOD], "m", lazy.spawn(TERMINAL + ' -e "ncmpcpp"')),
+        ([], "XF86AudioLowerVolume", lazy.spawn("pamixer --decrease 5")),
+        ([], "XF86AudioRaiseVolume", lazy.spawn("pamixer --increase 5")),
+        ([], "XF86AudioMute", lazy.spawn("pamixer --toggle-mute")),
+        (["shift"], "F12", lazy.spawn("pamixer --decrease 5")),
+        (["shift"], "F11", lazy.spawn("pamixer --increase 5")),
+        ([], "Print", lazy.spawn("flameshot gui")),
+        # Scratchpad toggles
+        # ("M-<slash>", lazy.group['scratchpad'].dropdown_toggle('term')),
+        # ("M-S-<slash>", lazy.group['scratchpad'].dropdown_toggle('ipython')),
+        ## ("M-<slash>", lazy.window.function(to_scratchpad)),
+        ## ("M-S-<slash>", lazy.function(show_scratchpad)),
+        ## .: Layout / Focus Manipulation :. #
+        ([MOD], "f", lazy.window.toggle_fullscreen()),
+        # Toggle between the available layouts.
+        ([MOD], "Tab", lazy.next_layout()),
+        ([MOD, "shift"], "Tab", lazy.prev_layout()),
+        # Close the current window: NO WARNING!
+        ([MOD], "w", lazy.window.kill()),
+        ## .: Sys + Utils :. #
+        # Restart qtile in place and pull in config changes (check config before
+        # doing this with `check-qtile-conf` script to avoid crashes)
+        ([MOD, "control"], "r", lazy.restart()),
+        # Shut down qtile.
+        ([MOD, "control"], "Escape", lazy.shutdown()),
+        ([MOD, "control"], "l", lazy.spawn("betterlockscreen --off 15 -t 'LOCKED' -l")),
+        ([MOD], "space", lazy.function(toggle_klayout))
+        # ([MOD,"shift"],"Delete", lazy.spawn(script("power-menu.sh"))),
+    ]
+]
 
 # .: Jump between groups and also throw windows to groups :. #
 for ix, group in enumerate(groups, 1):
-    keys.extend([Key(k[0], k[1], k[2]) for k in [
-        # ("M-%d" % ix, lazy.group[group.name].toscreen()),
-        ([MOD], str(ix), lazy.group[group.name].toscreen()),
-        # M-S-ix = switch to & move focused window to that group
-        ([MOD, "shift"], str(ix), lazy.window.togroup(group.name)),
-    ]])
+    keys.extend(
+        [
+            Key(k[0], k[1], k[2])
+            for k in [
+                # ("M-%d" % ix, lazy.group[group.name].toscreen()),
+                ([MOD], str(ix), lazy.group[group.name].toscreen()),
+                # M-S-ix = switch to & move focused window to that group
+                ([MOD, "shift"], str(ix), lazy.window.togroup(group.name)),
+            ]
+        ]
+    )
 
 # .: Use the mouse to drag floating layouts :. #
 mouse = [
-    Drag([MOD], "Button1", lazy.window.set_position_floating(),
-         start=lazy.window.get_position()),
-    Drag([MOD], "Button3", lazy.window.set_size_floating(),
-         start=lazy.window.get_size()),
-    Click([MOD], "Button2", lazy.window.bring_to_front())
+    Drag(
+        [MOD],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [MOD], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
+    Click([MOD], "Button2", lazy.window.bring_to_front()),
 ]
